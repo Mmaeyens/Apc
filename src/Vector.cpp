96b99e8bf545a125b1ebcd9c27052b52138b1d59
Matthias Maeyens <matthias.maeyens@student.kuleuven.be>
2016-07-06 16:44:43 +0200

    first verion not tested basic threads

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -2,6 +2,7 @@
 #include <omp.h>
 #include <fstream>
 #include <cstdlib>
+#include <mpi.h>
 using namespace std;
 
 Vector::Vector(int i, int j)
@@ -63,55 +64,85 @@ void Vector::writeFile(int name){
 }
 
 int Vector::iterateRed(){
-    int nthreads, tid;
-    #pragma omp parallel private(tid)
-    {
 
-    nthreads = omp_get_num_threads();
-    tid = omp_get_thread_num();
-    for (int i = 0; i < column/nthreads + (column%nthreads)/(tid+1); i++){
-        for (int j = 0; j < this->column; j++)
-        {
-            if (this->checkPos(i*nthreads + tid,j)==2){
-                if (this->checkPos(i*nthreads + tid,(j+1)%column) == 0)
-                {
-                    this->setPos(i*nthreads + tid,j,0);
-                    this->setPos(i*nthreads + tid,(j+1)%column,2);
-                    j++;
+    int nthreads, tid;
+    int tag = 666;
+    MPI_Comm_rank(MPI_COMM_WORLD, &tid);
+    MPI_Comm_size(MPI_COMM_WORLD, &nthreads);
+    for (int k=0; k < nthreads;k++){
+        if (k== tid){
+            for (int i = 0; i < column/nthreads + (column%nthreads)/(tid+1); i++){
+                for (int j = 0; j < this->column; j++){
+                    if (this->checkPos(i*nthreads + tid,j)==2){
+                        if (this->checkPos(i*nthreads + tid,(j+1)%column) == 0){
+                            this->setPos(i*nthreads + tid,j,0);
+                            this->setPos(i*nthreads + tid,(j+1)%column,2);
+                            j++;
+                        }
+                    }
                 }
             }
         }
-    }
     }
     return 1;
 }
 
 int Vector::iterateBlue(){
     int nthreads, tid;
-    /* Fork a team of threads with each thread having a private tid variable */
-    #pragma omp parallel private(tid)
-    {
-    nthreads = omp_get_num_threads();
-    tid = omp_get_thread_num();
-    for (int i = 0; i < column/nthreads + (column%nthreads)/(tid+1); i++){
-        for (int j = 0; j < row; j++)
-        {
-            if (this->checkPos(j,i*nthreads + tid)==1){
-                if (this->checkPos((j+1)%row,i*nthreads + tid) == 0)
+    int tag = 666;
+    MPI_Comm_rank(MPI_COMM_WORLD, &tid);
+    MPI_Comm_size(MPI_COMM_WORLD, &nthreads);
+    for (int k=0; k< nthreads;k++){
+        if (k== tid){
+            for (int i = 0; i < column/nthreads + (column%nthreads)/(tid+1); i++){
+                for (int j = 0; j < row; j++)
                 {
-                    this->setPos(j,i*nthreads + tid,0);
-                    this->setPos((j+1)%row,i*nthreads + tid,1);
+                    if (this->checkPos(j,i*nthreads + tid)==1){
+                        if (this->checkPos((j+1)%row,i*nthreads + tid) == 0)
+                        {
+                            this->setPos(j,i*nthreads + tid,0);
+                            this->setPos((j+1)%row,i*nthreads + tid,1);
+                            j++;
+                        }
                     j++;
+                    }
                 }
-            j++;
             }
         }
     }
-    }
     return 1;
 }
-
+//need different scatters for red and blue
+//Either make different vectors to store displacements for row or column
+//or go thread approach
 void Vector::iterate(vector<int> steps){
+    MPI_Init(NULL,NULL);
+
+//    int nthreads;
+//    MPI_Init (&argc, &argv);
+//    MPI_Comm_size(MPI_COMM_WORLD, &nthreads);
+//    int countscolumn[nthreads] = {0};
+//    int displacementscolumn[nthreads] = {0};
+//    for (int i = 0; i<column;i++){
+//        counts[i%nthreads] += column;
+//    }
+//    int sumcolumn = 0;
+//    for (int i = 0; i < nodes; i++){
+//        displacementscolumn[i] = sumcolumn;
+//        sumcolumn += countscolumn[i];
+//    }
+//    int countsrow[nthreads] = {0};
+//    int displacementsrow[nthreads] = {0};
+//    for (int i = 0; i<row;i++){
+//        counts[i%nthreads] += row;
+//    }
+//    int sumrow = 0;
+//    for (int i = 0; i < nodes; i++){
+//        displacementsrow[i] = sumrow;
+//        sumrow += countsrow[i];
+//    }
+//    int *rec_buf = new int[79];
+//    MPI_Scatterv(&arr, counts, displacements, MPI_INT, rec_buf, counts[tid], MPI_INT, 0, MPI_COMM_WORLD);
     int i = 0;
         for (int j = 0; j < steps[i]+1; j++){
             if (j == steps[i]){
@@ -132,6 +163,7 @@ void Vector::iterate(vector<int> steps){
             }
             this->iterateRed();
     }
+    MPI_Finalize();
 }
 void Vector::destroy(){
     delete arr;
