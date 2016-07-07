904c73947b00049468d51c7cfa8d9bb32a206b45
Matthias Maeyens <matthias.maeyens@student.kuleuven.be>
2016-07-07 16:11:19 +0200

    added check to see if first position is free

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -4,40 +4,48 @@
 #include <cstdlib>
 using namespace std;
 
-Vector::Vector(int i, int j)
+Vector::Vector(int newrow, int newcolumn)
 {
-    row = i;
-    column = j;
+    row = newrow;
+    column = newcolumn;
+    //create array
+    cout << row*column;
     arr = new int[row*column];
+
     string test;
     int k = 0;
     int h = 0;
-    int count;
-    ifstream  st("problem.csv");
+    int w;
+    ifstream  st("problem.csv",ios::in);
     getline(st,test);
     //read matrix with cars in
+
     while(getline(st,test))
     {
-        count = 0;
+        if (k ==0){
+        cout << test;}
+        w= 0;
         h = 0;
         for (int i = 0; i < test.size(); i++)
         {
             if (test[i] == ','){
-                this->arr[k*column + count] = atoi(test.substr(h,1).c_str());
+                arr[k*column + w] = atoi(test.substr(h,1).c_str());
                 h = i+1;
-                count++;
+                w++;
                 }
             }
-        this->arr[k*column + count] = atoi(test.substr(h,test.size()-h).c_str());
+        arr[k*column + w] = atoi(test.substr(h,test.size()-h).c_str());
         k++;
     }
-
+    st.clear();
+    st.close();
 }
+//set value at given position
 void Vector::setPos(int i,int j,int value){
     arr[column*i+j] = value;
     }
 
-
+// returnsvalue at given position
 int Vector::checkPos(int i,int j){
     return arr[column*i+j];
     }
@@ -68,18 +76,26 @@ int Vector::iterateRed(){
     {
 
     nthreads = omp_get_num_threads();
+
     tid = omp_get_thread_num();
-    for (int i = 0; i < column/nthreads + (column%nthreads)/(tid+1); i++){
+
+    for (int i = 0; i < row/nthreads + (row%nthreads)/(tid+1); i++){
+        bool firstisFree = (this->checkPos(i*nthreads + tid, 0) == 0);
         for (int j = 0; j < this->column; j++)
         {
             if (this->checkPos(i*nthreads + tid,j)==2){
-                if (this->checkPos(i*nthreads + tid,(j+1)%column) == 0)
+                if (j == this->column - 1 and !firstisFree) {
+                    j++;
+                }
+                else if (this->checkPos(i*nthreads + tid,(j+1)%column) == 0)
                 {
                     this->setPos(i*nthreads + tid,j,0);
                     this->setPos(i*nthreads + tid,(j+1)%column,2);
                     j++;
+
                 }
             }
+
         }
     }
     }
@@ -94,16 +110,20 @@ int Vector::iterateBlue(){
     nthreads = omp_get_num_threads();
     tid = omp_get_thread_num();
     for (int i = 0; i < column/nthreads + (column%nthreads)/(tid+1); i++){
+        bool firstisFree = (this->checkPos(0, i*nthreads + tid) == 0);
+
         for (int j = 0; j < row; j++)
         {
             if (this->checkPos(j,i*nthreads + tid)==1){
+                if (j == row - 1 and !firstisFree) {
+                    break;
+                }
                 if (this->checkPos((j+1)%row,i*nthreads + tid) == 0)
                 {
                     this->setPos(j,i*nthreads + tid,0);
                     this->setPos((j+1)%row,i*nthreads + tid,1);
                     j++;
                 }
-            j++;
             }
         }
     }
--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,11 @@
 using namespace std;
 // counts the commas in a string
 int count_comma(string s) {
-        int count = 0;
+        cout << s;
+        int w = 0;
         for (int i = 0; i < s.size(); i++)
-            if (s[i] == ',') count++;
-        return count;
+            if (s[i] == ',') w++;
+        return w;
 }
 
 
@@ -23,40 +24,49 @@ vector<int> parse(string info){
     grootte = grootte + 2;
     vector<int> steps(grootte);
     int h=0;
-    int count = 0;
+    int w= 0;
         for (int i = 0; i < info.size(); i++){
             if (info[i] == ',') {
-                steps[count] = atoi(info.substr(h,i-h).c_str());
+                steps[w] = atoi(info.substr(h,i-h).c_str());
                 h = i+1;
-                count++;
+                w++;
             }
         }
-        steps[count] = atoi(info.substr(h,info.size()-h).c_str());
+        steps[w] = atoi(info.substr(h,info.size()-h).c_str());
 
         return steps;
 
 }
+
 int main()
 {
+
     int i,j;
-    ifstream  data("problem.csv");
-    string line,info;
+    ifstream  data("problem.csv",ios::in);
+
+    string line,info,number;
     // Read the first line
     getline(data,info);
     i = 0;
+    j = 0;
     // Get the amount of columns and rows.
-    while(getline(data,line))
-    {
-        i+=1;
-        }
 
-    j = count_comma(line) + 1;
+    while(getline(data, line)){
+        istringstream streamline(line);
+        if (i == 0) {
+            while(getline(streamline, number, ',')){
+                ++j;
+            }
+        }
+        ++i;
+    }
     data.clear();
     data.close();
     //Get steps from the first line
     vector<int> steps = parse(info);
     //Create vector
     Vector vec(i,j);
+
     //Iterate over vector
     vec.iterate(steps);
     vec.destroy();
